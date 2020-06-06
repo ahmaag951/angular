7f01202146b905d401875bb2e69f30adaf5a1742
ahmaag951 <thisismyway_2011@yahoo.com>
2020-06-06 02:39:46 +0200

    first commit

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'this titile is changed by ahmad ezzat';
+
+  course = {
+    title: "The Complete Angular Cource",
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  }
+
+}
